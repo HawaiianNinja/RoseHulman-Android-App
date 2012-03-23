9b4ecf74a6efd9e572c9bf3001a945b1b89d2d04
Andrew Hopkins <andrewjohnhopkins@gmail.com>
2012-03-23 13:33:15 -0400

    changed a few thing

--- src/awesome/app/RoseAndroidAppActivity.java
+++ src/awesome/app/RoseAndroidAppActivity.java
@@ -3,8 +3,6 @@ package awesome.app;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 
