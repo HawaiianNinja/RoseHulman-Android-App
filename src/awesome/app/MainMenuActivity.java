2eb9d35d96a5bb550547e1945e78ebbc53cbdb7e
Andrew Hopkins <andrewjohnhopkins@gmail.com>
2012-03-23 15:29:44 -0400

    renamed some stuff to make more sense

---
+++ src/awesome/app/MainMenuActivity.java
@@ -0,0 +1,61 @@
+package awesome.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MainMenuActivity extends Activity implements OnClickListener {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		((View) findViewById(R.id.schedule_lookup_button))
+				.setOnClickListener(this);
+		((View) findViewById(R.id.student_lookup_button))
+				.setOnClickListener(this);
+		((View) findViewById(R.id.ara_menu_button)).setOnClickListener(this);
+		((View) findViewById(R.id.help_button)).setOnClickListener(this);
+		((View) findViewById(R.id.feedback_button)).setOnClickListener(this);
+		((View) findViewById(R.id.bandwidth_button)).setOnClickListener(this);
+	}
+
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		super.onCreateOptionsMenu(menu);
+//		MenuInflater inflater = getMenuInflater();
+//		inflater.inflate(R.menu.main_menu, menu);
+//		return true;
+//	}
+
+	public void onClick(View v) {
+		Intent intent = null;
+		switch (v.getId()) {
+		case R.id.feedback_button:
+			intent = new Intent(this, InAppFeedback.class);
+			startActivity(intent);
+			break;
+		case R.id.student_lookup_button:
+			intent = new Intent(this, StudentLookupActivity.class);
+			startActivity(intent);
+			break;
+		case R.id.ara_menu_button:
+			Intent araIntent = new Intent(this, AraMenuActivity.class);
+			startActivity(araIntent);
+			break;
+		case R.id.help_button:
+			Intent helpIntent = new Intent(this, HelpActivity.class);
+			startActivity(helpIntent);
+			break;
+		case R.id.schedule_lookup_button:
+			intent = new Intent(this, ScheduleLookupActivity.class);
+			startActivity(intent);
+			break;
+		case R.id.bandwidth_button:
+			intent = new Intent(this, BandwidthActivity.class);
+			startActivity(intent);
+			break;
+		}
+	}
+}
