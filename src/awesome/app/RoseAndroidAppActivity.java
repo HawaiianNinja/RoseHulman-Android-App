72b31d2b3b830f29147681bbd4082ec5094beeaa
Andrew Hopkins <hopkinaj@rose-hulman.edu>
2012-04-05 22:13:10 -0400

    Started the password manager

--- src/awesome/app/RoseAndroidAppActivity.java
+++ src/awesome/app/RoseAndroidAppActivity.java
@@ -1,32 +1,71 @@
 package awesome.app;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 
 public class RoseAndroidAppActivity extends Activity implements OnClickListener {
+	private final int PASSWORD_DIALOG = 1;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		((View) findViewById(R.id.schedule_lookup_button))
-				.setOnClickListener(this);
-		((View) findViewById(R.id.student_lookup_button))
-				.setOnClickListener(this);
+		((View) findViewById(R.id.schedule_lookup_button)).setOnClickListener(this);
+		((View) findViewById(R.id.student_lookup_button)).setOnClickListener(this);
 		((View) findViewById(R.id.ara_menu_button)).setOnClickListener(this);
 		((View) findViewById(R.id.help_button)).setOnClickListener(this);
 		((View) findViewById(R.id.feedback_button)).setOnClickListener(this);
 		((View) findViewById(R.id.bandwidth_button)).setOnClickListener(this);
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.editButton:
+//			showDialog(PASSWORD_DIALOG);
+			Dialog dialog = new Dialog(this);
+			dialog.setContentView(R.layout.password_dialog);
+			dialog.show();
+			return true;
+		}
+		return false;
+	}
+//
 //	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//		super.onCreateOptionsMenu(menu);
-//		MenuInflater inflater = getMenuInflater();
-//		inflater.inflate(R.menu.main_menu, menu);
-//		return true;
+//	protected Dialog onCreateDialog(int id) {
+//		super.onCreateDialog(id);
+//		AlertDialog dialog = null;
+//		switch (id) {
+//		case PASSWORD_DIALOG:
+//			AlertDialog.Builder builder;
+//			Context mContext = getApplicationContext();
+//			LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+//			View layout = inflater.inflate(R.layout.password_dialog, (ViewGroup) findViewById(R.id.password_root));
+//			builder = new AlertDialog.Builder(mContext);
+//			builder.setView(layout);
+//			dialog = builder.create();
+//		}
+//		return dialog;
 //	}
 
 	public void onClick(View v) {
@@ -34,30 +73,23 @@ public class RoseAndroidAppActivity extends Activity implements OnClickListener 
 		switch (v.getId()) {
 		case R.id.feedback_button:
 			intent = new Intent(this, InAppFeedback.class);
-			startActivity(intent);
 			break;
 		case R.id.student_lookup_button:
 			intent = new Intent(this, StudentLookupActivity.class);
-			startActivity(intent);
 			break;
 		case R.id.ara_menu_button:
-			Intent araIntent = new Intent(this, Ara.class);
-			startActivity(araIntent);
+			intent = new Intent(this, Ara.class);
 			break;
 		case R.id.help_button:
-			Intent helpIntent = new Intent(this, Help.class);
-			startActivity(helpIntent);
+			intent = new Intent(this, Help.class);
 			break;
 		case R.id.schedule_lookup_button:
 			intent = new Intent(this, ScheduleLookupActivity.class);
-			startActivity(intent);
 			break;
 		case R.id.bandwidth_button:
 			intent = new Intent(this, Bandwidth.class);
-			startActivity(intent);
 			break;
-			default:
-				break;
 		}
+		startActivity(intent);
 	}
 }
